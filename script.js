1dad39c91b04fbac335617a5f419ce0f851ffd68
Casey Elzinga <casey.elzinga@gmail.com>
2020-01-26 20:34:28 -0700

    basic html, css and js functions set up.

---
+++ script.js
@@ -0,0 +1,35 @@
+let constantA = 0;
+let constantB;
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function solve(a, operator, b) {
+    switch (operator) {
+        case '+':
+            add(a,b);
+            break;
+        case '-':
+            subtract(a,b);
+            break;
+        case '/':
+            divide(a,b);
+            break;
+        case '*':
+            multiply(a,b);
+            break;
+    }
+}
